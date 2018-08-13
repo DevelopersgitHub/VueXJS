aaf085d90e453c5cd7b5f5f15ab6b7e329476e1e
mikhail.m <mikhaylov.m@cronix.ms>
2018-08-13 17:33:00 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <h1>{{count}}</h1>
+    <!--<h1>{{count}}</h1>
     <button @click="increment"></button>
     <button @click="decrement"></button>
     <br>
     {{todos}}
+    <button @click="showDoneTodo"></button>-->
+
 
     <hr>
-    <button @click="showDoneTodo"></button>
+    <ul>
+      <li v-for="(user, index) in infoUser" :key="index">
+        {{user.name.first}} -  {{user.cell}}
+      </li>
+    </ul>
+    <button @click="getUser"></button>
+    <hr>
+
     <router-view/>
   </div>
 </template>
@@ -15,6 +24,9 @@
 <script>
 
   import {mapState} from 'vuex';
+  import $ from "jquery";
+  import axios from 'axios';
+
   export default {
     name: 'App',
     data() {
@@ -22,19 +34,20 @@
         payload: {
           name: 'Tom',
           age: 25
-        }
+        },
+        users: [],
+        infoUser: null
       }
     },
-    computed:{
-    ...mapState({
+    computed: {
+      ...mapState({
         count: 'count',
         todos: 'todos'
       }),
-      bla(){
+      bla() {
         return this.$store.getters.doneTodo;
       }
-    }
-    ,
+    },
     methods: {
       increment() {
         this.$store.commit('increment');
@@ -47,6 +60,25 @@
       },
       showDoneTodo() {
         this.$store.commit('showDoneTodo', this.payload.age);
+      },
+      getUser() {
+       /* let self = this
+        $.ajax({
+          url: 'https://randomuser.me/api/',
+          dataType: 'json',
+          success(data) {
+            self.infoUser = data.results;
+            console.log(this.infoUser)
+          }
+        });*/
+
+       axios.get('https://randomuser.me/api/', {
+         params: {
+           results: 5,
+           gender: 'female'
+         }
+       })
+         .then(response => this.infoUser = response.data.results)
       }
     }
   }
