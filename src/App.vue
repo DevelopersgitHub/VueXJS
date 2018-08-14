25d0328ac7ac40138208dcf41f67cbee88d29610
mikhail.m <mikhaylov.m@cronix.ms>
2018-08-14 18:08:45 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,29 @@
     <li v-for="(user, index) in infoUser" :key="index">
       {{index}} - {{user.userId}} - {{user.title}}
     </li>
-    <hr>-->
+    <hr>
 
 
     <div>
       <input-one/>
       <input-two/>
         <result/>
-    </div>
+    </div>-->
+
+
+    <hr>
+    <input type="text" v-model="randomName" placeholder="Add data about yourself"/>
+    <input type="text" v-model="randomValue" placeholder="Value"/>
+    <input v-model="s_type" placeholder="Add your s_type"/>
+    <button @click="OnClick(randomName, randomValue, s_type)">+</button>
+    <p>{{dataUser.last_name}}</p>
+
+
+    <hr>
+    <!--<input v-model="s_type" placeholder="Add s_type"/>-->
+    <input v-model="s_type" placeholder="Add s_type"/>
+    <button type="button" @click="cc">add</button>
+    <hr>
 
     <router-view/>
   </div>
@@ -50,6 +65,16 @@
     name: 'App',
     data() {
       return {
+        randomName: '',
+        randomValue: '',
+        s_type: '',
+        dataUser: {
+          id: 10,
+          name: 'Vladimir',
+          last_name: 'Borisov'
+        },
+        bool: false,
+
         payload: {
           name: 'Tom',
           age: 25
@@ -65,6 +90,25 @@
         show: true
       }
     },
+    watch: {
+     /* 'dataUser.name'(n){
+        console.log(n, 'name')
+      },
+      setTimeout(() => {
+             this.bool = true;
+           }, 1000);*/
+    /*else {
+          alert('Add correct s_type!');
+        }*/
+      'dataUser.sex'(value) {
+        console.log(value)
+        if (value.trim().toLowerCase() === 'male') {
+          this.dataUser.last_name = this.dataUser.last_name.slice(1, this.dataUser.last_name.length)
+        } else if (value.trim().toLowerCase() === 'female') {
+          this.dataUser.last_name = this.dataUser.last_name + 'a';
+        }
+      }
+    },
     computed: {
       ...mapState({
         count: 'count',
@@ -75,6 +119,18 @@
       }
     },
     methods: {
+      cc(){
+        this.$set(this.dataUser, 'sex', this.s_type);
+        // console.log(this.dataUser)
+      },
+      OnClick(randomName, randomValue, s_type) {
+        /*let randomField = randomName.value;
+        Object.assign(this.dataUser, {randomName: 'randomValue'});*/
+
+        this.dataUser[randomName] = randomValue;
+        this.dataUser.s_type = s_type;
+        console.log(this.dataUser);
+      },
       increment() {
         this.$store.commit('increment');
       },
